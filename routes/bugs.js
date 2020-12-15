077f216a5ef47f49085c92c67cb83fb71783aa43
Brody Faust <brodyfaust@brodys-air.lan>
2020-12-15 18:22:41 -0500

    Added bug post route

--- routes/bugs.js
+++ routes/bugs.js
@@ -3,6 +3,9 @@
 //~~~~~~~~~~~~~~~
 const bugsRouter = require('express').Router();
 
+//--- Schema Imports ---
+const Bug = require('../models/Bug');
+
 //~~~~~~~~~~~~~~~~~~
 //~~~ MIDDLEWARE ~~~
 //~~~~~~~~~~~~~~~~~~
@@ -21,6 +24,46 @@ bugsRouter.get('/:projectId', (req, res, next) => {});
 
 //Get a specific bug.
 bugsRouter.get('/:bugId', (req, res, next) => {});
+
+//--- POST Routes ---
+//Post a new bug to a given project.
+bugsRouter.post('/:projectId', async (req, res, next) => {
+	const bug = new Bug({
+		project_id: req.body.project_id,
+		title: req.body.title,
+		creator: req.body.creator,
+		createdAt: Date.now(),
+	});
+
+	console.log(bug);
+
+	try {
+		await bug.save(function (err) {
+			if (err) throw err;
+			res.end('Author successfully saved.');
+		});
+	} catch (error) {
+		res.status(404);
+		res.send({ message: error });
+	}
+	// bug
+	// 	.save()
+	// 	.then((data) => {
+	// 		res.json(data);
+	// 	})
+	// 	.catch((err) => {
+	// 		res.status(200);
+	// 		res.json({ message: err });
+	// 	});
+});
+
+//--- PATCH Routes ---
+//Updates a given bug's content.
+bugsRouter.patch('/:bugId', (req, res, next) => {});
+
+//--- DELETE Routes ---
+//Deletes a given bug.
+bugsRouter.delete('/:bugId', (req, res, next) => {});
 //~~~~~~~~~~~~~~~
 //~~~ EXPORTS ~~~
 //~~~~~~~~~~~~~~~
