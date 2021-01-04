d4bad338c81f4b7fa1397701ec745081054d6cf2
Brody Faust <brodyfaust@brodys-air.lan>
2021-01-03 19:10:54 -0500

    heroku deploy try 4

---
+++ build/precache-manifest.fad91b43e9d27c83e359977e4014474b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4b389287826c89c84e83efdeba15b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247c2b968ef147360c60",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a3945b8b43aa7cf39682",
+    "url": "/static/css/main.aa0e0c36.chunk.css"
+  },
+  {
+    "revision": "247c2b968ef147360c60",
+    "url": "/static/js/2.e5a84692.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/static/js/2.e5a84692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3945b8b43aa7cf39682",
+    "url": "/static/js/main.28cac964.chunk.js"
+  },
+  {
+    "revision": "96b7b594b80d1537a570",
+    "url": "/static/js/runtime-main.e8aa8433.js"
+  },
+  {
+    "revision": "1b9b9f71269e504156ce9d89a00f2551",
+    "url": "/static/media/github.1b9b9f71.png"
+  },
+  {
+    "revision": "1e244b9fb7dd7c8677d939c45cbfc8a5",
+    "url": "/static/media/logo4.1e244b9f.png"
+  },
+  {
+    "revision": "3485defb936b804b535d38e9b59c7f90",
+    "url": "/static/media/twitter.3485defb.png"
+  }
+]);
