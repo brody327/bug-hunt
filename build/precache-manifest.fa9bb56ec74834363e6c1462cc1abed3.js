d01147e5971d23b3bfd56edb887fe8c5f14bb029
Brody Faust <brodyfaust@brodys-air.lan>
2021-01-14 16:31:25 -0500

    Heroku deploy

--- build/precache-manifest.fa9bb56ec74834363e6c1462cc1abed3.js
+++ build/precache-manifest.fa9bb56ec74834363e6c1462cc1abed3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad4595dda6ddd3302cf2261b9b5ab12b",
+    "revision": "4c9577fb32ac58b7fffdb7d013cf8915",
     "url": "/index.html"
   },
   {
-    "revision": "4590b011ec230e83af54",
+    "revision": "cf6e5427d161e0af4acf",
     "url": "/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "7968badfaa8f7cf6ed9b",
+    "revision": "14f57fed6c28e721218c",
     "url": "/static/css/main.26a075f6.chunk.css"
   },
   {
-    "revision": "4590b011ec230e83af54",
-    "url": "/static/js/2.6726bca5.chunk.js"
+    "revision": "cf6e5427d161e0af4acf",
+    "url": "/static/js/2.fe5f3ffe.chunk.js"
   },
   {
     "revision": "6083d9c769f39b252a49c0c719a4b64c",
-    "url": "/static/js/2.6726bca5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.fe5f3ffe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7968badfaa8f7cf6ed9b",
-    "url": "/static/js/main.5729de8e.chunk.js"
+    "revision": "14f57fed6c28e721218c",
+    "url": "/static/js/main.bcb612eb.chunk.js"
   },
   {
     "revision": "96b7b594b80d1537a570",
